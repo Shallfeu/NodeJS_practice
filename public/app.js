461b230bba07c8944ce75476b457b1b27f3b0df9
Shallfeu <chigvincevra@mail.ru>
2022-11-26 13:53:47 +0300

    implemented edit possibility

--- public/app.js
+++ public/app.js
@@ -9,14 +9,46 @@ document.addEventListener('click', event => {
 })
 
 document.addEventListener('click', event => {
-    if (event.target.dataset.type === 'edit') {
-        const data = window.prompt('How would you like to edit note?')
+    if (event.target.dataset.type === 'save') {
         const id = event.target.dataset.id
-        if (data) {
-            edit({id, data}).then(() => {
-                location.reload();
-            })
-        }
+        const data = event.target.closest('li').querySelector('.input').value
+        edit({id ,data}).then(() => {
+            location.reload();
+        })
+    }
+})
+
+document.addEventListener('click', event => {
+    if (event.target.dataset.type === 'cancel') {
+        location.reload();
+    }
+})
+
+document.addEventListener('click', event => {
+    if (event.target.dataset.type === 'update') {
+        const id = event.target.dataset.id
+        // Button
+        const div =  document.createElement('div')
+        div.className = 'buttons'
+        const saveBtn = document.createElement('btn')
+        saveBtn.innerHTML = 'Save'
+        saveBtn.className = 'btn btn-success'
+        saveBtn.dataset.type = 'save'
+        saveBtn.dataset.id = id
+        const cancelBtn = document.createElement('btn')
+        cancelBtn.innerHTML = 'Cancel'
+        cancelBtn.className = 'btn btn-danger'
+        cancelBtn.dataset.type = 'cancel'
+        cancelBtn.dataset.id = id
+        div.appendChild(saveBtn)
+        div.appendChild(cancelBtn)
+        // Input
+        const input = document.createElement('input')
+        input.className = 'input'
+
+        event.target.closest('li').querySelector('.title').replaceWith(input)
+        event.target.closest('div').replaceWith(div)
+
     }
 })
 
@@ -25,6 +57,7 @@ async function remove(id) {
 }
 
 async function edit({id, data}) {
+    console.log(data)
     const requestOptions = {
         method: 'PUT',
         headers: {
